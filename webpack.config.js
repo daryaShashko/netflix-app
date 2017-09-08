184993c2061bf583986984169b7f3db4762dc7a4
daryaShashko <shashko.darya@gmail.com>
2017-09-08 16:03:41 +0300

    first task in progrees with errors

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+//const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: {
+    home: './client/src/client.js',
+    styles: './client/src/less/style.less'
+  },
+
+  output: {
+    path: __dirname + "public",
+    filename: '[name].js'
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  module: {
+
+    rules: [{
+      test: /\.less$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'less-loader']
+      })
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          plugins: ["transform-react-jsx"]
+        }
+      }
+    }
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Test',
+      hash: true,
+      template: './client/src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
+    })
+  ],
+  watch: true
+}
+;
